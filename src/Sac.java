c6417f7faeb44db45be6a8640d2d32dfa230801b
Clément De Coninck <clement.deconinck@apprenant.ifapme.be>
2025-11-18 14:40:14 +0100

    ArrayList pour Sac et début Chevalet

--- src/Sac.java
+++ src/Sac.java
@@ -2,59 +2,86 @@ import java.util.*;
 
 public class Sac {
 
-    Tuile[] sac = new Tuile[102];
+    ArrayList<Tuile> sac = new ArrayList<>();
 
     public Sac() {
 
-        int compteLettre = 0;
-
         // Lettres à 1 point
-        for (int i = 0; i < 15; i++) sac[compteLettre++] = new Tuile('E', 1);
-        for (int i = 0; i < 9; i++)  sac[compteLettre++] = new Tuile('A', 1);
-        for (int i = 0; i < 8; i++)  sac[compteLettre++] = new Tuile('I', 1);
-        for (int i = 0; i < 6; i++)  sac[compteLettre++] = new Tuile('N', 1);
-        for (int i = 0; i < 6; i++)  sac[compteLettre++] = new Tuile('O', 1);
-        for (int i = 0; i < 6; i++)  sac[compteLettre++] = new Tuile('R', 1);
-        for (int i = 0; i < 6; i++)  sac[compteLettre++] = new Tuile('S', 1);
-        for (int i = 0; i < 6; i++)  sac[compteLettre++] = new Tuile('T', 1);
-        for (int i = 0; i < 6; i++)  sac[compteLettre++] = new Tuile('U', 1);
-        for (int i = 0; i < 6; i++)  sac[compteLettre++] = new Tuile('L', 1);
+        for (int i = 0; i < 15; i++) sac.add(new Tuile('E', 1));
+        for (int i = 0; i < 9; i++)  sac.add(new Tuile('A', 1));
+        for (int i = 0; i < 8; i++)  sac.add(new Tuile('I', 1));
+        for (int i = 0; i < 6; i++)  sac.add(new Tuile('N', 1));
+        for (int i = 0; i < 6; i++)  sac.add(new Tuile('O', 1));
+        for (int i = 0; i < 6; i++)  sac.add(new Tuile('R', 1));
+        for (int i = 0; i < 6; i++)  sac.add(new Tuile('S', 1));
+        for (int i = 0; i < 6; i++)  sac.add(new Tuile('T', 1));
+        for (int i = 0; i < 6; i++)  sac.add(new Tuile('U', 1));
+        for (int i = 0; i < 5; i++)  sac.add(new Tuile('L', 1));
 
         // Lettres à 2 points
-        for (int i = 0; i < 3; i++) sac[compteLettre++] = new Tuile('D', 2);
-        for (int i = 0; i < 2; i++) sac[compteLettre++] = new Tuile('G', 2);
-        for (int i = 0; i < 3; i++) sac[compteLettre++] = new Tuile('M', 2);
+        for (int i = 0; i < 3; i++) sac.add(new Tuile('D', 2));
+        for (int i = 0; i < 2; i++) sac.add(new Tuile('G', 2));
+        for (int i = 0; i < 3; i++) sac.add(new Tuile('M', 2));
 
         // Lettres à 3 points
-        for (int i = 0; i < 2; i++) sac[compteLettre++] = new Tuile('B', 3);
-        for (int i = 0; i < 2; i++) sac[compteLettre++] = new Tuile('C', 3);
-        for (int i = 0; i < 2; i++) sac[compteLettre++] = new Tuile('P', 3);
+        for (int i = 0; i < 2; i++) sac.add(new Tuile('B', 3));
+        for (int i = 0; i < 2; i++) sac.add(new Tuile('C', 3));
+        for (int i = 0; i < 2; i++) sac.add(new Tuile('P', 3));
 
         // Lettres à 4 points
-        for (int i = 0; i < 2; i++) sac[compteLettre++] = new Tuile('F', 4);
-        for (int i = 0; i < 2; i++) sac[compteLettre++] = new Tuile('H', 4);
-        for (int i = 0; i < 2; i++) sac[compteLettre++] = new Tuile('V', 4);
+        for (int i = 0; i < 2; i++) sac.add(new Tuile('F', 4));
+        for (int i = 0; i < 2; i++) sac.add(new Tuile('H', 4));
+        for (int i = 0; i < 2; i++) sac.add(new Tuile('V', 4));
 
         // Lettres à 8 points
-        sac[compteLettre++] = new Tuile('J', 8);
-        sac[compteLettre++] = new Tuile('Q', 8);
+        sac.add(new Tuile('J', 8));
+        sac.add(new Tuile('Q', 8));
 
         // Lettres à 10 points
-        sac[compteLettre++] = new Tuile('K', 10);
-        sac[compteLettre++] = new Tuile('W', 10);
-        sac[compteLettre++] = new Tuile('X', 10);
-        sac[compteLettre++] = new Tuile('Y', 10);
-        sac[compteLettre++] = new Tuile('Z', 10);
+        sac.add(new Tuile('K', 10));
+        sac.add(new Tuile('W', 10));
+        sac.add(new Tuile('X', 10));
+        sac.add(new Tuile('Y', 10));
+        sac.add(new Tuile('Z', 10));
 
-        // Jokers (à ajouter dans la version 2.0)
+        System.out.println("Tuiles remplies : " + sac.size());
     }
 
-    public void piocher(Scanner sc) {
 
+    public void afficherSac() {
+        for (int i = 0; i < sac.size(); i++) {
+            Tuile t = sac.get(i);
+            System.out.println(i + " : " + t.lettre + " (" + t.valeur + ")");
+        }
+    }
+
+    public Tuile piocher() {
         Random rand = new Random();
 
-        for (int i = 0; i < sac.length; i++) {
-
+        if (sac.isEmpty()) {
+            System.out.println("Le sac est vide !");
+            return null;
         }
+
+        int index = rand.nextInt(sac.size());
+        Tuile pioche = sac.get(index);
+        sac.remove(index);
+
+        System.out.println("Tu as pioché : " + pioche.lettre + " (" + pioche.valeur + ")");
+
+        return pioche;
+    }
+
+    public static void main(String[] args) {
+//        Sac sac = new Sac();
+//        sac.afficherSac();
+//
+//        System.out.println("\nPiochage :");
+//        sac.piocher();
+//        sac.piocher();
+//        sac.piocher();
+//
+//        System.out.println("\nSac après piochage :");
+//        sac.afficherSac();
     }
 }
