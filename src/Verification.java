ec52db062515b73a0c02f33879effec282c17bc3
Clément De Coninck <clement.deconinck@apprenant.ifapme.be>
2025-11-25 16:15:46 +0100

    ajout de méthode vide

---
+++ src/Verification.java
@@ -0,0 +1,12 @@
+public class Verification {
+
+    Dictionnaire dico;
+
+    public Verification() {
+        this.dico = new Dictionnaire();
+    }
+
+    public void VerificationMot(Dictionnaire dico) {
+
+    }
+}
